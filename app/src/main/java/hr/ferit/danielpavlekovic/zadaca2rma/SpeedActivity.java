4512d0d735d24d7a309a75598fb79e90a57a0692
Potatocrunch94 <daniel.pavlekovic@gmail.com>
2017-03-28 10:20:32 +0200

    Activity Speed commit 1

--- app/src/main/java/hr/ferit/danielpavlekovic/zadaca2rma/SpeedActivity.java
+++ app/src/main/java/hr/ferit/danielpavlekovic/zadaca2rma/SpeedActivity.java
@@ -2,12 +2,25 @@ package hr.ferit.danielpavlekovic.zadaca2rma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SpeedActivity extends AppCompatActivity {
+
+    Spinner speedInput, speedOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speed);
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        speedInput = (Spinner) findViewById(R.id.spnSpeedInput);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.SpeedUnits, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        speedInput.setAdapter(adapter);
+        speedInput.setSelection(0);
     }
 }
