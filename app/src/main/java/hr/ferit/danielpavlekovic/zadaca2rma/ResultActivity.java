34eb1b5320c83aac520c9c6a3d12c75bca35a7d3
Potatocrunch94 <daniel.pavlekovic@gmail.com>
2017-03-28 09:27:03 +0200

    First commit

---
+++ app/src/main/java/hr/ferit/danielpavlekovic/zadaca2rma/ResultActivity.java
@@ -0,0 +1,13 @@
+package hr.ferit.danielpavlekovic.zadaca2rma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
